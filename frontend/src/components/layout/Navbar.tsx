c00267988b9d66b1563f9384fe43320ec1aed362
matthias.bouloc <matthias.bouloc@univ-rouen.fr>
2026-04-28 14:01:17 +0200

    fix(navbar): rendre la navbar desktop sticky au scroll

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -22,7 +22,7 @@ function DesktopNavbar() {
   };
 
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-base-100 shadow-sm sticky top-0 z-50">
       <div className="flex-1">
         <Link to="/" className="btn btn-ghost text-xl">
           TomManager
